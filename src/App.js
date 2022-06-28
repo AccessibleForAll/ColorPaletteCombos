4e302dad811c216375e14ae6f2da0d730fb130a1
Emma Dawson <emma.l.dawson@gmail.com>
2022-06-28 13:31:26 +0200

    Add color input section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import { useState } from 'react'
+
 import './App.css'
 
+import ColorInput from './components/ColorInput'
+
 function App() {
-  return <div className="App">Hello World</div>
+  const [colors, setColors] = useState([{ colorCode: '#ffffff' }, { colorCode: '#000000' }])
+
+  const addNewColorInput = () => {
+    setColors([...colors, { colorCode: '#' }])
+  }
+
+  const handleColorInput = (e, index) => {
+    let hexColor = e.target.value
+    if (!hexColor.startsWith('#')) {
+      hexColor = '#' + hexColor
+    }
+    setColors([...colors], (colors[index].colorCode = hexColor))
+  }
+  return (
+    <div className="appContainer">
+      <header>
+        <h1>Color Palette Combos</h1>
+      </header>
+      <main>
+        <section>
+          <p>Contrast ratio is the different in lightness between two colors.</p>
+          <p>Requirements according to WCAG 2.0:</p>
+          <ul>
+            <li>Level AA requires a ratio of 4.5:1 for normal text</li>
+            <li>Level AA requires a ratio of 3:1 for large text, graphics and user interface components</li>
+            <li>Level AAA requires a ratio of 7:1 for normal text</li>
+            <li>Level AAA requires a ratio of 4.5:1 for large text, graphics and user interface components</li>
+          </ul>
+          <p>Large text is defined as 24px with normal font weight or 18.66px with bold font weight.</p>
+        </section>
+        <section>
+          {colors.map((obj, i) => (
+            <ColorInput key={i} inputId={i + 1} bgColor={obj.colorCode} onChange={handleColorInput} />
+          ))}
+
+          {colors.length < 12 && <button onClick={addNewColorInput}>Add color</button>}
+        </section>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/ColorInput.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ColorInput = props => {
+  const { inputId, bgColor, onChange } = props
+  return (
+    <div className="colorInputContainer">
+      <div className="colorSwatch" style={{ backgroundColor: bgColor }}></div>
+      <div className="inputContainer">
+        <label htmlFor="colorInput">Color {inputId}:</label>
+        <input
+          type="text"
+          maxLength={7}
+          id={`colorInput-${inputId}`}
+          name="colorInput"
+          value={bgColor}
+          onChange={e => onChange(e, inputId - 1)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ColorInput
