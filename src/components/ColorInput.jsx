37a3ceb938bd4efbd382594f75d13f335d3201d1
Hat52 <tanolihamzaali@gmail.com>
2022-07-03 01:31:57 +0500

    aria-label added

--- src/components/ColorInput.jsx
+++ src/components/ColorInput.jsx
@@ -14,7 +14,7 @@ const ColorInput = ({ inputId,colorsLength, bgColor, onChange,handleDelete }) =>
           value={bgColor}
           onChange={(e) => onChange(e, inputId - 1)}
         />
-        {(colorsLength > 2)?<button className="deleteButton"  onClick={()=>handleDelete(inputId-1)}>
+        {(colorsLength > 2)?<button className="deleteButton" aria-label="delete input" onClick={()=>handleDelete(inputId-1)}>
         <AiOutlineCloseCircle color="grey" /></button>:null}
       </div>
     </div>
